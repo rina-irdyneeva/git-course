6b15572649cb086f9b8c44d2d51dff140428ec88
Rina Irdyneeva <rinairdyneeva@gmail.com>
2023-11-18 17:40:25 +0300

    sql-bolt db init script. Added table employees

--- sql-bolt-db-init.sql
+++ sql-bolt-db-init.sql
@@ -85,3 +85,27 @@ VALUES
 ('1w', 32),
 ('2e', 16),
 ('2w', 20);
+
+-- Main table for the lesson 7
+CREATE TABLE IF NOT EXISTS Employees (
+role VARCHAR(50) NOT NULL,
+name VARCHAR(30) NOT NULL,
+building VARCHAR(2) NOT NULL,
+years_employed INTEGER NOT NULL);
+
+-- Employees's data
+INSERT INTO Employees (role, name, building, years_employed)
+VALUES
+('Engineer', 'Becky A.', '1e', 4),
+('Engineer', 'Dan B.', '1e', 2),
+('Engineer', 'Sharon F.', '1e', 6),
+('Engineer', 'Dan M.', '1e', 4),
+('Engineer', 'Malcom S.', '1e', 1),
+('Artist', 'Tylar S.', '2w', 2),
+('Artist', 'Sherman D.', '2w', 8),
+('Artist', 'Jakob J.', '2w', 6),
+('Artist', 'Lillia A.', '2w', 7),
+('Artist', 'Brandon J.', '2w', 7),
+('Manager', 'Scott K.', '1e', 9),
+('Manager', 'Shirlee M.', '1e', 3),
+('Manager', 'Daria O.', '2w', 6);
